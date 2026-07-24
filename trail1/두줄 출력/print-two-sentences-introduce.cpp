252643def04f93adfac94ddf800bfb25ca8274b7
pio5232 <prioewoe@naver.com>
2026-07-24 23:20:26 +0900

    [Passed] 두줄 출력 (2ms, 0MB)

---
+++ trail1/두줄 출력/print-two-sentences-introduce.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello students!\n";
+    std::cout << "Welcome to LeebrosCode!\n";
+    // Please write your code here.
+    return 0;
+}
